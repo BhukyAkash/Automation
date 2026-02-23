1686a628d842f534d7ccf61fda0fbc6f7cf07eff
BhukyAkash <akashbhukya2002@gmail.com>
2026-02-23 15:32:29 +0530

    Data

--- pages/excel_utils.py
+++ pages/excel_utils.py
@@ -9,8 +9,8 @@ def get_vehicle_data():
     sheet = wb.active
 
     data = {
-        "vehicle_reg_no": sheet["A14"].value,
-        "mykad": sheet["B14"].value
+        "vehicle_reg_no": sheet["A6"].value,
+        "mykad": sheet["B6"].value
     
     }
     return data

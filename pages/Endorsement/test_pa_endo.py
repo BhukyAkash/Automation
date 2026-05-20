ed2630e9d76f5aa1ecba908777e1cb7b6f281faa
BhukyAkash <akashbhukya2002@gmail..com>
2026-05-20 14:53:45 +0530

    PA endo

--- pages/Endorsement/test_pa_endo.py
+++ pages/Endorsement/test_pa_endo.py
@@ -1,3 +1,4 @@
+import pytest
 from tips_endo import endorsement
 
 def test_pa(page, request):
@@ -6,9 +7,8 @@ def test_pa(page, request):
 
 
 
-
-        
     finally:
         page.bring_to_front()
+        page.get_by_text("playwright", exact=True).click()
         page.get_by_text("Sign Out", exact=True).click()
         page.wait_for_timeout(15000)

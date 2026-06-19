71ab44bc2aac1437ab3a2eb29608bee8f8153a12
BhukyAkash <akashbhukya2002@gmail..com>
2026-06-19 14:54:31 +0530

    PC, MC, & CV vehicle details added

--- pages/NB/vehicle_info.py
+++ pages/NB/vehicle_info.py
@@ -4,7 +4,7 @@ VEHICLE_INFO = {
         "place_of_use": "Melaka",
 
         # ---- Make / Model / Year ----
-        "change_vehicle": False,
+        "change_vehicle": True,
         "make": "BMW",
         "model": "M4",
         "year": "2020",
@@ -14,8 +14,60 @@ VEHICLE_INFO = {
         "seating_capacity": "2",
 
         # ---- Coverage ----
+        "change_coverage": True,
+        "coverage_type": "TP, Fire & Theft",  # "Comprehensive" or "TP, Fire & Theft"
+    },
+    "MC": {
+        # ---- Vehicle Search ----
+        "place_of_use": "Melaka",
+
+        # ---- Make / Model / Year ----
+        "change_vehicle": True,
+        "make": "HONDA",
+        "model": "SL",
+        "year": "2020",
+
+        # ---- Capacities ----
+        "engine_capacity": "125",
+        "seating_capacity": "2",
+
+        # ---- Coverage ----
         "change_coverage": False,
         "coverage_type": "Comprehensive",  # "Comprehensive" or "TP, Fire & Theft"
+    },
+    "CV": {
+        # ---- Vehicle Search ----
+        "place_of_use": "Melaka",
+
+        # ---- Vehicle Class / Use ----
+        "vehicle_class": "Commercial Vehicle",
+        "vehicle_use": "C permit",
+
+        # ---- Engine / Chassis ----
+        "engine_no": "63547",
+        "chassis_no": "34576657",
+
+        # ---- Make / Model / Year ----
+        "make": "VOLVO",
+        "model": "F16",
+        "year": "2015",
+        "variant": "NA",
+
+        # ---- Capacities ----
+        "engine_capacity": "1200",
+        "seating_capacity": "5",
+        "carrying_capacity": "20",
+        "carrying_capacity_unit": "Tonnes",  # "Tonnes" or "Kg"
+
+        # ---- Carriage Goods ----
+        "carriage_goods": "Beverages Bottles",
+
+        # ---- Coverage ----
+        "change_coverage": True,
+        "coverage_type": "TP, Fire & Theft",  # "Comprehensive" or "TP, Fire & Theft"
+
+        # --- Sum Insured ---
+        "sum_insured": "25000"
     }
 }
 
@@ -27,3 +79,25 @@ def get_vehicle_info(vehicle_type: str) -> dict:
             f"Valid types: {list(VEHICLE_INFO.keys())}"
         )
     return VEHICLE_INFO[vehicle_type]
+
+
+AUTOMATION_FLAGS = {
+
+    "MC": {
+        "explore_extensions":   True,   # Click on Extension Coverage button?
+        "select_autobuddy":     False,   # Select Motorcyclist PA Contract?
+        "select_extensions":    False,   # Select individual extensions?
+    },
+
+    "PC": {
+        "explore_extensions":   True,   # Click on Extension Coverage button?
+        "select_autobuddy":     False,  # Select Autobuddy package?
+        "select_extensions":    False,   # Select individual extensions?
+    },
+
+    "CV": {
+        "explore_extensions":   True,  # Click on Extension Coverage button?
+        "select_autobuddy":     False,  # Select Motorist PA package?
+        "select_extensions":    False,  # Select individual extensions?
+    },
+}
